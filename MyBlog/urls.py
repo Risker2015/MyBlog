f7e6fff3d830956edb870dee06bb5004050b9468
Risker2015 <1273814472@qq.com>
2017-12-20 10:34:04 +0800

    update comment

--- MyBlog/urls.py
+++ MyBlog/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+#//fdsfdsfdsfsdfdsf
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grzx/', include('grzx.urls', namespace='grzx')),
